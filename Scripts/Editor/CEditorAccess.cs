5083a346719b8cc670314436b0ef4f22d351f12d
sd.lee <sd.lee@ninetap.com>
2020-12-31 13:44:24 +0900

    [이상동] 활성 객체 반환 함수 수정

--- Scripts/Editor/CEditorAccess.cs
+++ Scripts/Editor/CEditorAccess.cs
@@ -24,9 +24,9 @@ public static partial class CEditorAccess {
 	//! 활성 객체를 반환한다
 	public static GameObject GetActiveObj(bool a_bIsInHierarchy = true) {
 		var oObj = Selection.activeGameObject;
-		bool bIsInvalidObj = a_bIsInHierarchy && !oObj.activeInHierarchy;
+		bool bIsInvalidObj = a_bIsInHierarchy && (oObj != null && !oObj.activeInHierarchy);
 
-		return (bIsInvalidObj || oObj == null) ? null : oObj;
+		return bIsInvalidObj ? null : oObj;
 	}
 
 	//! 독립 플랫폼 이름을 반환한다

f9ed1eb4f74f0cbe385c0c05aa2b2f71acd56f9a
dante <are2341@nate.com>
2021-02-15 15:19:26 +0900

    [이상동] iOS 관련 함수 추가

--- Scripts/Editor/CEditorAccess.cs
+++ Scripts/Editor/CEditorAccess.cs
@@ -32,6 +32,11 @@ public static partial class CEditorAccess {
 		return (a_eType == EStandaloneType.WNDS) ? KCDefine.B_PLATFORM_N_WNDS : KCDefine.B_PLATFORM_N_MAC;
 	}
 
+	//! iOS 이름을 반환한다
+	public static string GetiOSName(EiOSType a_eType) {
+		return KCDefine.B_PLATFORM_N_APPLE;
+	}
+
 	//! 안드로이드 이름을 반환한다
 	public static string GetAndroidName(EAndroidType a_eType) {
 		// 원 스토어 일 경우

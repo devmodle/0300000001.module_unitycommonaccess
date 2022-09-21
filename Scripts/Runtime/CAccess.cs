7f45db9191cc117a07caf9efef17201ddac4634e
sd.lee <sd.lee@ninetap.com>
2022-09-21 18:54:54 +0900

    [이상동]

--- Scripts/Runtime/CAccess.cs
+++ Scripts/Runtime/CAccess.cs
@@ -41,15 +41,15 @@ public static partial class CAccess {
 	}
 
 	/** 쓰기용 스트림을 반환한다 */
-	public static FileStream GetWriteStream(string a_oFilePath, bool a_bIsAutoCreateDir = true) {
+	public static FileStream GetWriteStream(string a_oFilePath) {
 		CAccess.Assert(a_oFilePath.ExIsValid());
 		string oDirPath = Path.GetDirectoryName(a_oFilePath).Replace(KCDefine.B_TOKEN_REV_SLASH, KCDefine.B_TOKEN_SLASH);
 
-		// 디렉토리 자동 생성 모드 일 경우
-		if(a_bIsAutoCreateDir && oDirPath.ExIsValid() && !Directory.Exists(oDirPath)) {
+		// 디렉토리가 없을 경우
+		if(oDirPath.ExIsValid() && !Directory.Exists(oDirPath)) {
 			Directory.CreateDirectory(oDirPath);
 		}
-		
+
 		return File.Open(a_oFilePath, FileMode.Create, FileAccess.Write);
 	}
 

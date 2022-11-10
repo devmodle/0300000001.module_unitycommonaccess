54849b25184657daf7b1c18ec947b6dd839c93a0
dante <lkstudio.dante@gmail.com>
2022-11-10 14:14:51 +0900

    [이상동] 코드 서식 갱신

--- Scripts/Editor/CEditorAccess.cs
+++ Scripts/Editor/CEditorAccess.cs
@@ -12,9 +12,11 @@ using UnityEngine.SceneManagement;
 
 /** 에디터 기본 접근 */
 [InitializeOnLoad]
-public static partial class CEditorAccess {
+public static partial class CEditorAccess
+{
 	/** 식별자 */
-	private enum EKey {
+	private enum EKey
+	{
 		NONE = -1,
 		IS_IMPORT_ASSETS,
 		[HideInInspector] MAX_VAL
@@ -30,7 +32,8 @@ public static partial class CEditorAccess {
 
 	#region 클래스 함수
 	/** 생성자 */
-	static CEditorAccess() {
+	static CEditorAccess()
+	{
 		AssetDatabase.importPackageStarted -= CEditorAccess.OnStartAssetImport;
 		AssetDatabase.importPackageStarted += CEditorAccess.OnStartAssetImport;
 
@@ -45,18 +48,21 @@ public static partial class CEditorAccess {
 	}
 
 	/** 에셋 존재 여부를 검사한다 */
-	public static bool IsExistsAsset(string a_oFilePath) {
+	public static bool IsExistsAsset(string a_oFilePath)
+	{
 		return AssetDatabase.GetMainAssetTypeAtPath(a_oFilePath) != null;
 	}
 
 	/** 전처리기 심볼 포함 여부를 검사한다 */
-	public static bool IsContainsDefineSymbol(BuildTargetGroup a_eTargetGroup, string a_oSymbol) {
+	public static bool IsContainsDefineSymbol(BuildTargetGroup a_eTargetGroup, string a_oSymbol)
+	{
 		PlayerSettings.GetScriptingDefineSymbolsForGroup(a_eTargetGroup, out string[] oDefineSymbols);
 		return oDefineSymbols.ExIsValid() && oDefineSymbols.ExIsContains(a_oSymbol);
 	}
 
 	/** 최상단 객체 프리팹 경로를 반환한다 */
-	public static string GetRootObjPrefabPath(Scene a_stScene, string a_oName) {
+	public static string GetRootObjPrefabPath(Scene a_stScene, string a_oName)
+	{
 		string oDirPath = (!a_stScene.name.Contains(KCDefine.B_EDITOR_SCENE_N_PATTERN_01) && !a_stScene.name.Contains(KCDefine.B_EDITOR_SCENE_N_PATTERN_02)) ? KCEditorDefine.B_DIR_P_SUB_UNITY_PROJ_PREFABS : KCEditorDefine.B_DIR_P_SUB_UNITY_PROJ_EDITOR_PREFABS;
 		string oRootPath = (Directory.GetParent(a_stScene.path) != null) ? Directory.GetParent(a_stScene.path).Name.ExGetFileName(false) : string.Empty;
 		string oPrefabPath = (oRootPath.ExIsValid() && !oRootPath.Equals(KCDefine.B_DIR_N_SCENES)) ? string.Format(KCDefine.B_TEXT_FMT_4_SLASH_COMBINE, Path.GetDirectoryName(oDirPath).Replace(KCDefine.B_TOKEN_REV_SLASH, KCDefine.B_TOKEN_SLASH), CEditorAccess.GetSceneDirName(a_stScene), oRootPath, string.Format(KCDefine.B_TEXT_FMT_2_UNDER_SCORE_COMBINE, a_stScene.name, a_oName)) : string.Format(KCDefine.B_TEXT_FMT_3_SLASH_COMBINE, Path.GetDirectoryName(oDirPath).Replace(KCDefine.B_TOKEN_REV_SLASH, KCDefine.B_TOKEN_SLASH), CEditorAccess.GetSceneDirName(a_stScene), string.Format(KCDefine.B_TEXT_FMT_2_UNDER_SCORE_COMBINE, a_stScene.name, a_oName));
@@ -65,89 +71,111 @@ public static partial class CEditorAccess {
 	}
 
 	/** 활성 객체를 반환한다 */
-	public static GameObject GetActiveObj(bool a_bIsInHierarchy = true) {
+	public static GameObject GetActiveObj(bool a_bIsInHierarchy = true)
+	{
 		return (a_bIsInHierarchy && (Selection.activeGameObject != null && !Selection.activeGameObject.activeInHierarchy)) ? null : Selection.activeGameObject;
 	}
 
 	/** 씬 디렉토리 이름을 반환한다 */
-	public static string GetSceneDirName(Scene a_stScene) {
+	public static string GetSceneDirName(Scene a_stScene)
+	{
 		return KCEditorDefine.B_DIR_N_SCENE_DICT.GetValueOrDefault(a_stScene.name, (!a_stScene.name.Contains(KCDefine.B_EDITOR_SCENE_N_PATTERN_01) && !a_stScene.name.Contains(KCDefine.B_EDITOR_SCENE_N_PATTERN_02)) ? Path.GetDirectoryName(KCEditorDefine.B_DIR_P_SUB_UNITY_PROJ).Replace(KCDefine.B_TOKEN_REV_SLASH, KCDefine.B_TOKEN_SLASH) : Path.GetDirectoryName(KCEditorDefine.B_DIR_P_SUB_UNITY_PROJ_EDITOR).Replace(KCDefine.B_TOKEN_REV_SLASH, KCDefine.B_TOKEN_SLASH));
 	}
 
 	/** iOS 프로젝트 이름을 반환한다 */
-	public static string GetiOSProjName(EiOSType a_eType) {
+	public static string GetiOSProjName(EiOSType a_eType)
+	{
 		return KCEditorDefine.B_IOS_APPLE_BUILD_PROJ_N_JENKINS;
 	}
 
 	/** iOS 빌드 결과 경로를 반환한다 */
-	public static string GetiOSBuildOutputPath(EiOSType a_eType, string a_oBuildFileExtension) {
+	public static string GetiOSBuildOutputPath(EiOSType a_eType, string a_oBuildFileExtension)
+	{
 		return string.Format(KCEditorDefine.B_BUILD_OUTPUT_P_FMT_IOS, CAccess.GetiOSName(a_eType), a_oBuildFileExtension);
 	}
 
 	/** 안드로이드 프로젝트 이름을 반환한다 */
-	public static string GetAndroidProjName(EAndroidType a_eType) {
-		switch(a_eType) {
-			case EAndroidType.AMAZON: return KCEditorDefine.B_ANDROID_AMAZON_BUILD_PROJ_N_JENKINS;
+	public static string GetAndroidProjName(EAndroidType a_eType)
+	{
+		switch(a_eType)
+		{
+			case EAndroidType.AMAZON:
+				return KCEditorDefine.B_ANDROID_AMAZON_BUILD_PROJ_N_JENKINS;
 		}
 
 		return KCEditorDefine.B_ANDROID_GOOGLE_BUILD_PROJ_N_JENKINS;
 	}
 
 	/** 안드로이드 빌드 결과 경로를 반환한다 */
-	public static string GetAndroidBuildOutputPath(EAndroidType a_eType, string a_oBuildFileExtension) {
+	public static string GetAndroidBuildOutputPath(EAndroidType a_eType, string a_oBuildFileExtension)
+	{
 		return string.Format(KCEditorDefine.B_BUILD_OUTPUT_P_FMT_ANDROID, CAccess.GetAndroidName(a_eType), a_oBuildFileExtension);
 	}
 
 	/** 독립 플랫폼 프로젝트 이름을 반환한다 */
-	public static string GetStandaloneProjName(EStandaloneType a_eType) {
-		switch(a_eType) {
-			case EStandaloneType.WNDS_STEAM: return KCEditorDefine.B_STANDALONE_WNDS_STEAM_BUILD_PROJ_N_JENKINS;
+	public static string GetStandaloneProjName(EStandaloneType a_eType)
+	{
+		switch(a_eType)
+		{
+			case EStandaloneType.WNDS_STEAM:
+				return KCEditorDefine.B_STANDALONE_WNDS_STEAM_BUILD_PROJ_N_JENKINS;
 		}
 
 		return KCEditorDefine.B_STANDALONE_MAC_STEAM_BUILD_PROJ_N_JENKINS;
 	}
 
 	/** 독립 플랫폼 빌드 결과 경로를 반환한다 */
-	public static string GetStandaloneBuildOutputPath(EStandaloneType a_eType, string a_oBuildFileExtension) {
+	public static string GetStandaloneBuildOutputPath(EStandaloneType a_eType, string a_oBuildFileExtension)
+	{
 		return string.Format(KCEditorDefine.B_BUILD_OUTPUT_P_FMT_STANDALONE, CAccess.GetStandaloneName(a_eType), a_oBuildFileExtension);
 	}
 
 	/** 그래픽 API 를 변경한다 */
-	public static void SetGraphicAPI(BuildTarget a_eTarget, List<GraphicsDeviceType> a_oDeviceTypeList, bool a_bIsEnableAuto = true, bool a_bIsEnableAssert = true) {
+	public static void SetGraphicAPI(BuildTarget a_eTarget, List<GraphicsDeviceType> a_oDeviceTypeList, bool a_bIsEnableAuto = true, bool a_bIsEnableAssert = true)
+	{
 		CAccess.Assert(!a_bIsEnableAssert || a_oDeviceTypeList.ExIsValid());
 
 		// 디바이스 타입이 존재할 경우
-		if(a_oDeviceTypeList.ExIsValid()) {
+		if(a_oDeviceTypeList.ExIsValid())
+		{
 			PlayerSettings.SetGraphicsAPIs(a_eTarget, a_oDeviceTypeList.ToArray());
 			PlayerSettings.SetUseDefaultGraphicsAPIs(a_eTarget, a_bIsEnableAuto);
 		}
 	}
 
 	/** 에셋 임포트가 시작했을 경우 */
-	private static void OnStartAssetImport(string a_oAssetName) {
+	private static void OnStartAssetImport(string a_oAssetName)
+	{
 		CEditorAccess.m_oBoolDict.ExReplaceVal(EKey.IS_IMPORT_ASSETS, true);
 	}
 
 	/** 에셋 임포트가 완료 되었을 경우 */
-	private static void OnCompleteAssetImport(string a_oAssetName) {
+	private static void OnCompleteAssetImport(string a_oAssetName)
+	{
 		CEditorAccess.m_oBoolDict.ExReplaceVal(EKey.IS_IMPORT_ASSETS, false);
 	}
 
 	/** 에셋 임포트가 실패했을 경우 */
-	private static void OnFailAssetImport(string a_oAssetName, string a_oErrorMsg) {
+	private static void OnFailAssetImport(string a_oAssetName, string a_oErrorMsg)
+	{
 		CEditorAccess.m_oBoolDict.ExReplaceVal(EKey.IS_IMPORT_ASSETS, false);
 	}
 
 	/** 값을 대체한다 */
-	private static void ExReplaceVal<K, V>(this Dictionary<K, V> a_oSender, K a_tKey, V a_tVal, bool a_bIsEnableAssert = true) {
+	private static void ExReplaceVal<K, V>(this Dictionary<K, V> a_oSender, K a_tKey, V a_tVal, bool a_bIsEnableAssert = true)
+	{
 		CAccess.Assert(!a_bIsEnableAssert || a_oSender != null);
 
 		// 딕셔너리가 존재 할 경우
-		if(a_oSender != null) {
+		if(a_oSender != null)
+		{
 			// 값 대체가 가능 할 경우
-			if(a_oSender.ContainsKey(a_tKey)) {
+			if(a_oSender.ContainsKey(a_tKey))
+			{
 				a_oSender[a_tKey] = a_tVal;
-			} else {
+			}
+			else
+			{
 				a_oSender.Add(a_tKey, a_tVal);
 			}
 		}

1cd5ad985f1b40e729afca83a71f291e0b52981d
dante <lkstudio.dante@gmail.com>
2022-11-12 13:50:01 +0900

    [이상동] 코드 형식 갱신

--- Scripts/Editor/CEditorAccessExtension.cs
+++ Scripts/Editor/CEditorAccessExtension.cs
@@ -15,44 +15,36 @@ using UnityEditor.iOS.Xcode;
 #endif            // #if UNITY_IOS                          
 
 /** 에디터 기본 접근자 확장 클래스 */
-public static partial class CEditorAccessExtension
-{
+public static partial class CEditorAccessExtension {
 	#region 클래스 함수
 	/** 유효 여부를 검사한다 */
-	public static bool ExIsValid(this EBuildMode a_eSender)
-	{
+	public static bool ExIsValid(this EBuildMode a_eSender) {
 		return a_eSender > EBuildMode.NONE && a_eSender < EBuildMode.MAX_VAL;
 	}
 
 	/** 유효 여부를 검사한다 */
-	public static bool ExIsValid(this BuildTarget a_eSender)
-	{
+	public static bool ExIsValid(this BuildTarget a_eSender) {
 		bool bIsMobile = a_eSender == BuildTarget.iOS || a_eSender == BuildTarget.Android;
 		return bIsMobile || (a_eSender == BuildTarget.StandaloneOSX || a_eSender == BuildTarget.StandaloneWindows || a_eSender == BuildTarget.StandaloneWindows64);
 	}
 
 	/** 성공 완료 여부를 검사한다 */
-	public static bool ExIsCompleteSuccess(this Request a_oSender)
-	{
+	public static bool ExIsCompleteSuccess(this Request a_oSender) {
 		CAccess.Assert(a_oSender != null);
 		return a_oSender.IsCompleted && a_oSender.Status == StatusCode.Success;
 	}
 
 	/** 정적 에디터 플래그를 설정한다 */
-	public static void ExSetStaticEditorFlags(this GameObject a_oSender, StaticEditorFlags a_eFlags, bool a_bIsEnableAssert = true)
-	{
+	public static void ExSetStaticEditorFlags(this GameObject a_oSender, StaticEditorFlags a_eFlags, bool a_bIsEnableAssert = true) {
 		CAccess.Assert(!a_bIsEnableAssert || a_oSender != null);
 
 		// 객체가 존재 할 경우
-		if(a_oSender != null)
-		{
+		if(a_oSender != null) {
 			var oEnumerator = a_oSender.DescendantsAndSelf();
 
-			foreach(var oObj in oEnumerator)
-			{
+			foreach(var oObj in oEnumerator) {
 				// 플래그 설정이 필요 할 경우
-				if(GameObjectUtility.GetStaticEditorFlags(oObj) != a_eFlags)
-				{
+				if(GameObjectUtility.GetStaticEditorFlags(oObj) != a_eFlags) {
 					GameObjectUtility.SetStaticEditorFlags(oObj, a_eFlags);
 				}
 			}
--- Scripts/Runtime/CAccess.cs
+++ Scripts/Runtime/CAccess.cs
@@ -8,8 +8,7 @@ using UnityEngine.UI;
 using UnityEngine.Events;
 
 /** 기본 접근자 */
-public static partial class CAccess
-{
+public static partial class CAccess {
 	#region 클래스 프로퍼티
 	public static bool IsSupportMSAA => SystemInfo.supportsMultisampledTextures > KCDefine.B_VAL_0_INT;
 	public static string MidnightDeltaTimeStr => new System.DateTime(CAccess.MidnightDeltaTime.Ticks).ToString(KCDefine.B_DATE_T_FMT_HH_MM_SS);
@@ -20,11 +19,9 @@ public static partial class CAccess
 
 	#region 클래스 함수
 	/** 유저 문자열을 반환한다 */
-	public static string GetUserStr(EUserType a_eUserType)
-	{
+	public static string GetUserStr(EUserType a_eUserType) {
 		// 유저 타입이 유효하지 않을 경우
-		if(!a_eUserType.ExIsValid())
-		{
+		if(!a_eUserType.ExIsValid()) {
 			return KCDefine.B_TOKEN_USER_UNKNOWN;
 		}
 
@@ -32,28 +29,24 @@ public static partial class CAccess
 	}
 
 	/** 버전 문자열을 반환한다 */
-	public static string GetVerStr(string a_oVer, EUserType a_eUserType)
-	{
+	public static string GetVerStr(string a_oVer, EUserType a_eUserType) {
 		string oUserStr = CAccess.GetUserStr(a_eUserType);
 		return string.Format(KCDefine.B_TEXT_FMT_VER, a_oVer, oUserStr);
 	}
 
 	/** 읽기용 스트림을 반환한다 */
-	public static FileStream GetReadStream(string a_oFilePath)
-	{
+	public static FileStream GetReadStream(string a_oFilePath) {
 		CAccess.Assert(a_oFilePath.ExIsValid());
 		return File.Exists(a_oFilePath) ? File.Open(a_oFilePath, FileMode.Open, FileAccess.Read) : null;
 	}
 
 	/** 쓰기용 스트림을 반환한다 */
-	public static FileStream GetWriteStream(string a_oFilePath)
-	{
+	public static FileStream GetWriteStream(string a_oFilePath) {
 		CAccess.Assert(a_oFilePath.ExIsValid());
 		string oDirPath = Path.GetDirectoryName(a_oFilePath).Replace(KCDefine.B_TOKEN_REV_SLASH, KCDefine.B_TOKEN_SLASH);
 
 		// 디렉토리가 없을 경우
-		if(oDirPath.ExIsValid() && !Directory.Exists(oDirPath))
-		{
+		if(oDirPath.ExIsValid() && !Directory.Exists(oDirPath)) {
 			Directory.CreateDirectory(oDirPath);
 		}
 
@@ -61,20 +54,17 @@ public static partial class CAccess
 	}
 
 	/** 랜덤 확률을 반환한다 */
-	public static (int, float) GetRandPercent(List<float> a_oPercentList)
-	{
+	public static (int, float) GetRandPercent(List<float> a_oPercentList) {
 		CAccess.Assert(a_oPercentList.ExIsValid());
 
 		float fPercent = Random.Range(KCDefine.B_VAL_0_REAL, a_oPercentList.Sum((a_fPercent) => a_fPercent));
 		float fComparePercent = KCDefine.B_VAL_0_REAL;
 
-		for(int i = 0; i < a_oPercentList.Count - KCDefine.B_VAL_1_INT; ++i)
-		{
+		for(int i = 0; i < a_oPercentList.Count - KCDefine.B_VAL_1_INT; ++i) {
 			fComparePercent += Mathf.Abs(a_oPercentList[i]);
 
 			// 확률을 만족 할 경우
-			if(fPercent.ExIsLessEquals(fComparePercent) && !Mathf.Abs(a_oPercentList[i]).ExIsEquals(KCDefine.B_VAL_0_REAL))
-			{
+			if(fPercent.ExIsLessEquals(fComparePercent) && !Mathf.Abs(a_oPercentList[i]).ExIsEquals(KCDefine.B_VAL_0_REAL)) {
 				return (i, a_oPercentList[i]);
 			}
 		}
@@ -84,17 +74,14 @@ public static partial class CAccess
 
 	/** 조건을 검사한다 */
 	[Conditional("DEBUG"), Conditional("DEVELOPMENT_BUILD")]
-	public static void Assert(bool a_bIsTrue)
-	{
+	public static void Assert(bool a_bIsTrue) {
 		UnityEngine.Assertions.Assert.IsTrue(a_bIsTrue);
 	}
 
 	/** 값을 비교한다 */
-	private static int ExCompare(this float a_fSender, float a_fRhs)
-	{
+	private static int ExCompare(this float a_fSender, float a_fRhs) {
 		// 값이 동일 할 경우
-		if(a_fSender.ExIsEquals(a_fRhs))
-		{
+		if(a_fSender.ExIsEquals(a_fRhs)) {
 			return KCDefine.B_COMPARE_EQUALS;
 		}
 
@@ -104,19 +91,16 @@ public static partial class CAccess
 
 	#region 제네릭 클래스 함수
 	/** 열거형 값을 반환한다 */
-	public static List<T> GetEnumVals<T>()
-	{
+	public static List<T> GetEnumVals<T>() {
 		return (System.Enum.GetValues(typeof(T)) as T[]).ToList();
 	}
 
 	/** 열거형 문자열을 반환한다 */
-	public static List<string> GetEnumStrs<T>()
-	{
+	public static List<string> GetEnumStrs<T>() {
 		var oEnumStrList = new List<string>();
 		var oEnumValList = CAccess.GetEnumVals<T>();
 
-		for(int i = 0; i < oEnumValList.Count; ++i)
-		{
+		for(int i = 0; i < oEnumValList.Count; ++i) {
 			oEnumStrList.Add(oEnumValList[i].ToString());
 		}
 

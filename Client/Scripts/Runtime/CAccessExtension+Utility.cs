04c3c58f98491eda06a4821ccb8a3dc486f9f0d7
dante <lkstudio.dante@gmail.com>
2023-03-02 16:27:00 +0900

    [이상동] 이벤트 관련 구문 수정

--- Client/Scripts/Runtime/CAccessExtension+Utility.cs
+++ Client/Scripts/Runtime/CAccessExtension+Utility.cs
@@ -710,13 +710,20 @@ public static partial class CAccessExtension {
 		}
 	}
 
-	/** 레이어를 설정한다 */
-	public static void ExSetLayer(this GameObject a_oSender, int a_nLayer, bool a_bIsEnableAssert = true) {
+	/** 레이어를 변경한다 */
+	public static void ExSetLayer(this GameObject a_oSender, int a_nLayer, bool a_bIsResetChildren = true, bool a_bIsEnableAssert = true) {
 		CAccess.Assert(!a_bIsEnableAssert || a_oSender != null);
 
 		// 객체가 존재 할 경우
 		if(a_oSender != null) {
 			a_oSender.layer = a_nLayer;
+
+			// 자식 객체 리셋 모드 일 경우
+			if(a_bIsResetChildren) {
+				foreach(var oObj in a_oSender.Descendants()) {
+					oObj.layer = a_nLayer;
+				}
+			}
 		}
 	}
 
